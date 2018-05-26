ce93d118c2fe3a994c6a922f1f2b85bc660d8628
montedonioluiz <montedonio.luiz@gmail.com>
2018-05-25 21:01:58 -0300

    update

--- Connection.py
+++ Connection.py
@@ -1,15 +1,15 @@
 from flaskext.mysql import MySQL
 
-mysql = MySQL()
 
-def config_connection(main):
+
+def get_cursor(main):
     main.config['MYSQL_DATABASE_USER'] = 'root'
     main.config['MYSQL_DATABASE_PASSWORD'] = 'root'
     main.config['MYSQL_DATABASE_DB'] = 'supermercado'
-    connect(main)
 
-def connect(main):
+    mysql = MySQL(main)
     mysql.init_app(main)
 
-def get_cursor():
-    return mysql.get_db().cursor()
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    return mysql, conn, cursor
--- DBComands.py
+++ DBComands.py
@@ -1,18 +1,17 @@
 from flask import Flask, request, render_template
 from Connection import *
+from flaskext.mysql import MySQL
 
 db_comands = Flask(__name__)
-config_connection(db_comands)
-cursor = get_cursor()
 
 @db_comands.route('/')
 def index():
     return render_template('login.html')
 
 
-@db_comands.route('/servlogin')
-
+@db_comands.route('/servlogin', methods=['POST'])
 def servlogin():
+    mysql, conn, cursor = get_cursor(db_comands)
     name = request.args.get('name')
     password = request.args.get('password')
 
@@ -22,21 +21,22 @@ def servlogin():
     mysql.connect().close()
     return listar_produtos()
 
-def listar_produtos():
 
+def listar_produtos():
+    mysql, conn, cursor = get_cursor(db_comands)
     cursor.execute('select * from products')
     produtos = cursor.fetchall() #dictionary
+    mysql.connect().close()
     return render_template('/listarprodutos.html', produtos = produtos)
+
 
 @db_comands.route('/detalhar')
 def detalhar():
-    cursor = get_cursor
-
+    mysql, conn, cursor = get_cursor(db_comands)
     id = request.args.get('id')
-
     cursor.execute(f'select * from products WHERE id = {id}')
     produto = cursor.fetchone()
-    print(produto)
+    mysql.connect().close()
     return render_template('/detalharproduto.html', produto = produto)
 
 
